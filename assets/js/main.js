0af80649a616e74d75431a099a79310b1deb6f7a
Taleh Rzayev <taleh.rzayev.i@asoiu.edu.az>
2023-12-04 12:54:05 +0400

    update step 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,157 +1,146 @@
-const jobsBlock = document.querySelector(".jobs-block");
-const jobsHead = document.querySelector("#jobs-head");
+/* Variables */
+
+const jobsBlock = document.querySelector('.jobs-block');
+const theme = document.querySelector('#theme-check');
+const body = document.querySelector('body')
+const jobsTitle = document.querySelector('#jobs-head');
 const jobsLocation = document.querySelector("#jobs-location");
-const jobsType = document.querySelector("#jobs-type");
-const theme = document.querySelector("#theme-check");
-const body = document.querySelector("body");
+const jobsType = document.querySelector('#jobs-type')
+let result = [];
 
 
 
+/*
 
-const data = [
-    {
-        id: 1,
-        img: './assets/img/jobs-1.svg',
-        time: '20h ago',
-        time_type: 'Full Time',
-        head: 'Haskell and PureScript Dev',
-        company: 'Blogr',
-        location: 'United States'
-    },
-    {
-        id: 2,
-        img: './assets/img/jobs-1.svg',
-        time: '20h11 ago',
-        time_type: 'Full Time',
-        head: 'MasterCode',
-        company: 'Blogr',
-        location: 'demo'
-    },
-    ,
-    {
-        id: 2,
-        img: './assets/img/jobs-1.svg',
-        time: '20h11 ago',
-        time_type: 'Part Time',
-        head: 'Haske',
-        company: 'Blogr',
-        location: 'demo'
-    }
-]
-let result = data;
+Step 2 Tasks
 
+- Search functionality 
+- Inner Page
+- Add Data
+- Update Data 
+- Delete Data
 
-function getCurrentUrl() {
-    let urlParams = new URLSearchParams(window.location.search);
-
-    let id = urlParams.get('id');
-
-    let pageName = window.location.pathname.split('/').pop();
-
-    return {
-        id: id,
-        pageName: pageName
-    };
-}
-
+*/
 
 
 window.onload = function () {
-
-    let pageName = getCurrentUrl();
     let pageTheme = localStorage.getItem('theme');
-
-    if(pageTheme=='dark'){
-        body.classList.add('dark');
-        theme.checked=true;
-    }
-
-
-
-    if (pageName.pageName == 'index.html') {
-        getData(data);
+    if (pageTheme == 'dark') {
+        body.classList.add('dark')
+        theme.checked = true;
     }
     else {
-        console.log(pageName);
+        body.classList.remove('dark')
     }
-};
+    getData(data);
+}
 
-function getData(data) {
+const data = [
+    {
+        id: '1',
+        title: 'Software Engineer',
+        img: './assets/img/jobs-1.svg',
+        time: '5h ago',
+        workType: 'Part Time',
+        company: 'Scoot',
+        location: 'Canada'
+    },
+    {
+        id: '2',
+        title: 'Senior Software Engineer 1',
+        img: './assets/img/jobs-1.svg',
+        time: '5h ago',
+        workType: 'Full Time',
+        company: 'Scoot',
+        location: 'United Kingdom'
+    },
+    {
+        id: '3',
+        title: 'Senior Software Engineer 2',
+        img: './assets/img/jobs-1.svg',
+        time: '5h ago',
+        workType: 'Full Time',
+        company: 'Scoot',
+        location: 'United Kingdom'
+    },
+]
 
-    dataHtml = '';
+function getData(a) {
+    let dataHtml = '';
 
-    data.map((item) => {
+    a.map((item) => {
         dataHtml += `
-    <div class="jobs-item">
-    <div class="jobs-item_img">
-        <img src="${item.img}" alt="">
-    </div>
-    <div class="jobs-item_body">
-        <div class="jobs-item_info">
-            <p>${item.time}</p>
-            <p>${item.time_type}</p>
+        <div class="jobs-item">
+        <div class="jobs-item_img">
+            <img src="${item.img}" alt="">
         </div>
-        <a href="/inner.html?${item.id}" class="jobs-item_head">
-            ${item.head}
-        </a>
-        <div class="jobs-item_company">
-            ${item.company}
+        <div class="jobs-item_body">
+            <div class="jobs-item_info">
+                <p>${item.time}</p>
+                <p>${item.workType}</p>
+            </div>
+            <a href="/inner.html?id=${item.id}" class="jobs-item_head">
+                ${item.title}
+            </a>
+            <div class="jobs-item_company">
+                ${item.company}
+            </div>
+        </div>
+        <div class="jobs-item_footer">
+            <p class="jobs-item_location">${item.location}</p>
         </div>
     </div>
-    <div class="jobs-item_footer">
-        <p class="jobs-item_location">${item.location}</p>
-    </div>
-</div>
-    `
+        `
     })
 
     jobsBlock.innerHTML = dataHtml;
+
 }
 
-function search(searchTerm, searchBy, data) {
+
+theme.addEventListener('click', (e) => {
+    if (e.target.checked) {
+        localStorage.setItem('theme', 'dark')
+    }
+    else {
+        localStorage.setItem('theme', 'light')
+    }
+
+    body.classList.toggle('dark');
+})
+
+
+
+function search(searchTerm, key, data) {
     searchTerm = searchTerm.toLowerCase();
-
-    const filteredData = data.filter(item => {
-        const currentItemValue = item[searchBy].toLowerCase();
+    const filteredData = data.filter((item) => {
+        const currentItemValue = item[key].toLowerCase();
+        console.log(currentItemValue);
         return currentItemValue.includes(searchTerm);
-    });
-
+    })
     return filteredData;
 }
 
 
-jobsHead?.addEventListener('keyup', (e) => {
-    result = search(e.target.value, "head", data);
+jobsTitle?.addEventListener('keyup', (e) => {
+    result = search(e.target.value, 'title', data)
     getData(result)
 })
-
 
 jobsLocation?.addEventListener('keyup', (e) => {
-    result = search(e.target.value, "location", result);
+    result = search(e.target.value, 'location', data)
     getData(result)
 })
+
+
 
 
 jobsType?.addEventListener('click', (e) => {
     if (e.target.checked) {
-        result = search("Full Time", "time_type", result);
+        result = search('Full Time', 'workType', data);
         getData(result)
     }
     else {
         getData(data)
     }
-})
-
-theme?.addEventListener('click', (e) => {
-
-    if (e.target.checked) {
-        console.log('dfg');
-        localStorage.setItem('theme','dark')
-    }
-    else {
-        localStorage.setItem('theme','light')
-    }
-
-    body.classList.toggle('dark')
-
 })
